b14c5ae8145600c200865c9f4fae8151c2bf49dd
Ryan-Seek <rdseek96@gmail.com>
2019-12-30 14:13:00 -0800

    Spreadsheet Application

---
+++ Spreadsheet/SpreadsheetEngine/cell.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace SpreadsheetEngine
+{
+    class cell : INotifyPropertyChanged 
+    {
+        private int rowIndex;
+        private int columnIndex;
+        protected string Text;
+
+
+        public int getRow
+        {
+            get { return rowIndex; }
+        }
+
+        public int getColumn
+        {
+            get { return columnIndex;}
+        }
+
+
+
+    }
+}
